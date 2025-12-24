c7f861e312ffd58434f74735b49ee698568fb875
Paul Koeck <paul@koeck.dev>
2025-12-24 23:46:14 +0100

    fix: ssh keys

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, pkgs, ... }:
 {
   programs.git = {
     enable = true;
@@ -13,6 +13,21 @@
       merge.conflictstyle = "diff3";
       diff.colorMoved = "default";
       color.ui = true;
+    };
+
+    extraConfig = {
+      gpg = {
+        format = "ssh";
+      };
+      "gpg \"ssh\"" = {
+        program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
+      };
+      commit = {
+        gpgsign = true;
+      };
+      user = {
+        signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDjUGpuR4RN77nMKZsANUGpurG/pVP2K+/aOx9cmIxR6";
+      };
     };
   };
 }
--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -2,6 +2,10 @@
 {
   programs.ssh = {
     enable = true;
+    extraConfig = ''
+      Host *
+          IdentityAgent ~/.1password/agent.sock
+    '';
   };
 
   services.ssh-agent.enable = true;
